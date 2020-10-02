f498dc32d9bd4efcec6494fbc1a86ac565240231
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-02 15:57:59 -0500

    added replayability option

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,89 @@
 // function that takes no argument but returns rock, paper, or scissors when called
 function computerPlay() {
     // array of choices to randomly choose from
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     // choose a choice randomly
     let computerSelection = choices[Math.floor(Math.random() * choices.length)];
     // return the chosen choice
     return computerSelection;
 }
 function game() {
-    // greet player
-    console.log("Welcome to the classic game of Rock, Paper, Scissors!")
+    let playerSelection;
+    let computerSelection;
+
     // prompt user for input
-    let playerSelection = prompt("Welcome to Rock, Paper, Scissors! \n Make your choice: Rock, Paper or Scissors");
+    playerSelection = prompt("Make your choice: Rock, Paper or Scissors"); 
+
     // run computer choice
+    computerSelection = computerPlay();
 
-    // compare user input to computer choice
-
-    // return the evaluation result to user
-
-    // record the score
+    // evaluate choices
+    evaluateChoices(computerSelection, playerSelection);
 
     // display current scores
-
-    // ask user if they want to keep playing
-
-    // if yes then replay
-
-    // if no then announce scores and thank for playing
+    console.log(`The current score is: Computer- ${computerScore} Player- ${playerScore}`);
+    return;
 }
 
-
+// function to evaluate choices and return the outcome
 function evaluateChoices (computerSelection, playerSelection) {
-
+    playerSelection = playerSelection.toLowerCase();
+    
+    // Rock beats scissors
+    if (computerSelection === "rock" & playerSelection === "scissors") {
+        console.log("You Lose! Rock beats Scissors");
+        computerScore += 1;
+    }
+    else if (computerSelection === "scissors" & playerSelection === "rock") {
+        console.log("You Win! Rock beats Scissors");
+        playerScore += 1;
+    }
+    // Paper beats rocks
+    else if (computerSelection === "paper" & playerSelection === "rock") {
+        console.log("You Lose! Paper beats Rock");
+        computerScore += 1;
+    }
+    else if (computerSelection === "rock" & playerSelection === "paper") {
+        console.log("You Win! Paper beats Rock");
+        playerScore += 1;
+    }
+    // Scissors beats paper
+    else if (computerSelection === "scissors" & playerSelection === "paper") {
+        console.log("You Lose! Scissors beats Paper");
+        computerScore += 1;
+    }
+    else {
+        console.log("You Win! Scissors beats Paper");
+        playerScore += 1;
+    }
+    return;
 }
 
-game();
+function keepPlayingPrompt() {
+        // ask user if they want to keep playing
+        keepPlayingSelection = prompt("Do you want to keep playing?").toLowerCase();
+        if (keepPlayingSelection === "yes") {
+            return;
+        }
+        else {
+            keepPlayingStatus = false;
+            console.log("Thanks for playing! See you next time!");
+        }
+}
+
+
+
+
+/// Program code
+let computerScore = 0;
+let playerScore = 0;
+let keepPlayingStatus = true;
+
+console.log("Welcome to the classic game of Rock, Paper, Scissors!");
+while (keepPlayingStatus === true){
+    game();
+    keepPlayingPrompt();
+}
+
 
 
