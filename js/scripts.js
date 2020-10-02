d34efdc863205eadc90ee5c18050205c62ecd43f
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-02 13:55:26 -0500

    wrote basic algorithm for game function

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,37 @@ function computerPlay() {
     // array of choices to randomly choose from
     let choices = ["Rock", "Paper", "Scissors"];
     // choose a choice randomly
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
     // return the chosen choice
-    return randomChoice;
+    return computerSelection;
+}
+function game() {
+    // greet player
+
+    // prompt user for input
+    let playerSelection = prompt("Welcome to Rock, Paper, Scissors! \n Make your choice: Rock, Paper or Scissors");
+    // run computer choice
+
+    // compare user input to computer choice
+
+    // return the evaluation result to user
+
+    // record the score
+
+    // display current scores
+
+    // ask user if they want to keep playing
+
+    // if yes then replay
+
+    // if no then announce scores and thank for playing
 }
 
 
+function evaluateChoices (computerSelection, playerSelection) {
+
+}
+
+game();
 
 
-console.log(computerPlay());
